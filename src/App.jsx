22cd8897c77dd2b6e97f334a85680efacddd24cc
gilmerperez <gilmer2002@outlook.com>
2025-02-04 21:34:37 -0500

    Polished routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Resume from './Resume';
+import './global.css';
+
 function App() {
   return (
-    <div>
-      <h1>My Portfolio</h1>
-      <p>Welcome to my React portfolio! More coming soon...</p>
-    </div>
-  )
+    <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App
