749fe87b2aecc1a97f9d9013d7537b19f3186c14
gilmerperez <gilmer2002@outlook.com>
2025-02-07 10:10:53 -0500

    Added validateEmail function to Contact.jsx instead of a seperate file

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { useState } from "react";
 import { validateEmail } from "../utils/helpers";
 
+export function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 function Form() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
