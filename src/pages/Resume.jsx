942f3296b3948003af2c0fa1ffe9dde500e92dd3
gilmerperez <gilmer2002@outlook.com>
2025-02-06 20:55:06 -0500

    Added Resume Page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,20 +3,48 @@ import React from 'react'
 const Resume = () => {
   return (
     <div>
-      <h1>My Resume</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <h1>Professional Work Resume</h1>
+
+      <h2>Gilmer Perez</h2>
+      <h4>123 Main Street, City, State 00000</h4>
+
+      <h5>Professional Summary:</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Laudantium rerum tempore sit nostrum adipisci esse velit voluptate excepturi, <br />ipsum repudiandae enim sequi voluptas nobis quas, quam sunt nam, cum id.</p>
+
+      <h5>Skills:</h5>
+      <ul>
+        <li>Skill 1</li>
+        <li>Skill 2</li>
+        <li>Skill 3</li>
+        <li>Skill 4</li>
+        <li>Skill 5</li>
+      </ul>
+
+      <h5>Education:</h5>
+      <p>Harvard University | Computer Science</p>
+      <p>Graduated December 2021</p>
+
+      <h5>Front-End Proficiencies:</h5>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>Responsive Design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+
+      <h5>Back-End Proficiencies</h5>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>PostgreSQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+      </ul>
     </div>
   )
 }
