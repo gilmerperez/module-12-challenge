6c5ea6d3f978b372d19819242cfd29d038366d99
gilmerperez <gilmer2002@outlook.com>
2025-02-07 00:17:17 -0500

    Added Font Awesome via CDN to index.html and placed GitHub, LinkedIn and Stack Overflow Logos in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">GitHub</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">LinkedIn</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Stack Overflow</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><i className="fab fa-github fa-2x"></i></a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><i className="fab fa-linkedin fa-2x"></i></a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><i className="fab fa-stack-overflow fa-2x"></i></a></li>
     </ul>
     <p class="text-center text-body-secondary">Copyright © 2025 Gilmer Perez</p>
   </footer>
