d815710c6320f60a9ecc24348c6330ae30b20b7f
gilmerperez <gilmer2002@outlook.com>
2025-02-07 10:18:37 -0500

    Removed import after adding validateEmail directly to Contact.jsx

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Error = () => {
   return (
-    <div className="container d-flex flex-column justify-content-center align-items-center vh-100 text-center">
+    <div className="container d-flex flex-column justify-content-center align-items-center vh-100 text-center py-5">
+      <h1 className="text-center mb-4">404 Page Not Found</h1>
+
       <h1>Oops!</h1>
       <p className="lead">Sorry, an unexpected error has occurred.</p>
-      <p className="text-muted"></p>
+      <i class="fa-solid fa-bug fa-3x"></i>
     </div>
   );
 };
