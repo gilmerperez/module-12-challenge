c1c3e3de20913b32777a82212489a71fe9b449d3
gilmerperez <gilmer2002@outlook.com>
2025-02-06 20:36:15 -0500

    Added Bootstrap Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">GitHub</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">LinkedIn</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Stack Overflow</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">Copyright © 2025 Gilmer Perez</p>
+  </footer>
   )
 }
 
