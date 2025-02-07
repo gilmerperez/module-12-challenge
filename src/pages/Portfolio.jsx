275ac7a98be3dc0b1211844924eb1163880f69ba
gilmerperez <gilmer2002@outlook.com>
2025-02-06 22:40:56 -0500

    Added Portfolio Page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,35 +1,64 @@
-import React from 'react'
+import React from "react";
 
 const Portfolio = () => {
+  const portfolioItems = [
+    {
+      title: "Project One",
+      image: "bootstrap-themes.png",
+      description: "A fantastic project using Bootstrap.",
+    },
+    {
+      title: "Project Two",
+      image: "bootstrap-themes.png",
+      description: "A great web development project.",
+    },
+    {
+      title: "Project Three",
+      image: "bootstrap-themes.png",
+      description: "A creative solution with responsive design.",
+    },
+    {
+      title: "Project Four",
+      image: "bootstrap-themes.png",
+      description: "A stunning portfolio example.",
+    },
+    {
+      title: "Project Five",
+      image: "bootstrap-themes.png",
+      description: "A modern, sleek, and fast UI.",
+    },
+    {
+      title: "Project Six",
+      image: "bootstrap-themes.png",
+      description: "A smooth and interactive user experience.",
+    },
+  ];
+
   return (
-    <div>
-      <h1>My Portfolio</h1>
-      <div>
-        <h2>Project 1</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, optio facilis cum illum pariatur officiis ut at atque molestias amet.</p>
-      </div>
-      <div>
-        <h2>Project 2</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, optio facilis cum illum pariatur officiis ut at atque molestias amet.</p>
-      </div>
-      <div>
-        <h2>Project 3</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, optio facilis cum illum pariatur officiis ut at atque molestias amet.</p>
-      </div>
-      <div>
-        <h2>Project 4</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, optio facilis cum illum pariatur officiis ut at atque molestias amet.</p>
-      </div>
-      <div>
-        <h2>Project 5</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, optio facilis cum illum pariatur officiis ut at atque molestias amet.</p>
-      </div>
-      <div>
-        <h2>Project 6</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, optio facilis cum illum pariatur officiis ut at atque molestias amet.</p>
+    <div className="container py-5">
+      <h1 className="text-center mb-4">My Portfolio</h1>
+
+      <div className="row">
+        {portfolioItems.map((item, index) => (
+          <div key={index} className="col-12 col-md-6 col-lg-6 mb-4">
+            <div className="card h-100 shadow-sm">
+              <img
+                src={item.image}
+                className="card-img-top"
+                alt={item.title}
+                loading="lazy"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{item.title}</h5>
+                <p className="card-text">{item.description}</p>
+                <button className="btn btn-primary">View Project</button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
