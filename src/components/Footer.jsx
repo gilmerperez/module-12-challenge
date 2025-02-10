28a7c5cd7f5ac6aa3a3fa56ff48b909def953b6b
gilmerperez <gilmer2002@outlook.com>
2025-02-09 19:16:32 -0500

    Added functional links in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,17 @@ const Footer = () => {
   return (
     <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><i className="fab fa-github fa-2x"></i></a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><i className="fab fa-linkedin fa-2x"></i></a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><i className="fab fa-stack-overflow fa-2x"></i></a></li>
+      <li class="nav-item">
+        <a href="https://github.com/" class="nav-link px-2 text-body-secondary" target="blank" attribute><i className="fab fa-github fa-2x"></i></a>
+      </li>
+
+      <li class="nav-item">
+        <a href="https://www.linkedin.com/" class="nav-link px-2 text-body-secondary" target="blank"><i className="fab fa-linkedin fa-2x"></i></a>
+      </li>
+
+      <li class="nav-item">
+        <a href="https://stackoverflow.com/questions" class="nav-link px-2 text-body-secondary" target="blank"><i className="fab fa-stack-overflow fa-2x"></i></a>
+      </li>
     </ul>
     <p class="text-center text-body-secondary">Copyright © 2025 Gilmer Perez</p>
   </footer>
